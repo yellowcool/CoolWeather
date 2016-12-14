97b97b18cb1b74fb1cf85c79c3bb7046905cca22
coolyellow <13406805102@163.com>
2016-12-14 15:31:03 +0800

    add baiduSDK loaction

--- app/src/main/java/test/collyellow/coolweather/fragment/WeatherFragment.java
+++ app/src/main/java/test/collyellow/coolweather/fragment/WeatherFragment.java
@@ -7,10 +7,15 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 
 import test.collyellow.coolweather.App;
 import test.collyellow.coolweather.R;
@@ -23,11 +28,25 @@ import test.collyellow.coolweather.databinding.WeatherFragmentBinding;
 
 public class WeatherFragment extends Fragment {
     private int y;
+    public LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
+    private WeatherFragmentBinding binding;
+
+    public class Presenter {
+        public void onClick(View view) {
+            Toast.makeText(App.context, "aaaaaa", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        final WeatherFragmentBinding binding = DataBindingUtil.inflate(inflater, R.layout.weather_fragment, container, false);
+        mLocationClient = new LocationClient(App.context);     //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);    //注册监听函数
+        initLocation();
+        mLocationClient.start();
+        binding = DataBindingUtil.inflate(inflater, R.layout.weather_fragment, container, false);
+        binding.setPresenter(new Presenter());
         binding.weatherFragmentRecyclerview.setLayoutManager(new LinearLayoutManager(App.context));
         binding.weatherFragmentRecyclerview.setAdapter(new WeatherFragmentRecyclerViewAdapter());
         binding.weatherFragmentRecyclerview.addOnScrollListener(new RecyclerView.OnScrollListener() {
@@ -36,23 +55,53 @@ public class WeatherFragment extends Fragment {
                 int height = binding.title.getHeight();
                 if (dy > 0) {
                     y += dy;
-                    Log.e("yyyy", "yyyy = " + y);
-                    if (y > height) {
-                        y = height;
+                    if (y > 200) {
+                        y = 200;
                     }
-                    int scale = (int) Math.abs((y * 1.0 / height) * 255);
-
-                    Log.e("scale", "scale = " + scale);
+                    int scale = (int) Math.abs((y * 1.0 / 200) * 255);
                     binding.title.setBackgroundColor(Color.argb((int) scale, 0, 0, 0));
                     int scale2 = scale / 3;
                     binding.weatherFragmentRecyclerview.setBackgroundColor(Color.argb(scale2, 0, 0, 0));
-                }
-                else {
-                    binding.title.setBackgroundColor(Color.argb(0, 0, 0, 0));
-                    binding.weatherFragmentRecyclerview.setBackgroundColor(Color.argb(0, 0, 0, 0));
+                } else {
+                    y += dy;
+                    if (y < 0) {
+                        y = 0;
+                    }
+                    int scale = (int) Math.abs((y * 1.0 / 200) * 255);
+                    binding.title.setBackgroundColor(Color.argb(scale, 0, 0, 0));
+                    int scale2 = scale / 3;
+                    binding.weatherFragmentRecyclerview.setBackgroundColor(Color.argb(scale2, 0, 0, 0));
                 }
             }
         });
         return binding.getRoot();
+    }
+
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span = 1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
+        mLocationClient.setLocOption(option);
+    }
+
+    public class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            String city = location.getCity();
+            if (city != null) {
+                binding.weatherFragmentRecyclerviewCity.setText(city);
+            }
+        }
     }
 }
