49f8a526cf27fc9f6bab53154364ea54508f2169
coolyellow <13406805102@163.com>
2016-12-14 16:49:09 +0800

    add recyclerview headerview

--- app/src/main/java/test/collyellow/coolweather/adapter/WeatherFragmentRecyclerViewAdapter.java
+++ app/src/main/java/test/collyellow/coolweather/adapter/WeatherFragmentRecyclerViewAdapter.java
@@ -16,9 +16,36 @@ import test.collyellow.coolweather.holder.WeatherFragmentRecyclerHolder;
  */
 
 public class WeatherFragmentRecyclerViewAdapter extends RecyclerView.Adapter<WeatherFragmentRecyclerHolder> {
+    private static int TYPE_ONE = 0;
+    private static int TYPE_SECONDE = 1;
+    private static int TYPE_THIRD = 2;
+    private static int TYPE_DEFALT = 3;
+    private ViewDataBinding binding;
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return TYPE_ONE;
+        } else if (position == 1) {
+            return TYPE_SECONDE;
+        } else if (position == 2) {
+            return TYPE_THIRD;
+        }else {
+            return TYPE_DEFALT;
+        }
+    }
+
     @Override
     public WeatherFragmentRecyclerHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        ViewDataBinding binding = DataBindingUtil.inflate(LayoutInflater.from(App.context), R.layout.weather_fragment_recycler_items, parent, false);
+        if (viewType == TYPE_ONE) {
+            binding = DataBindingUtil.inflate(LayoutInflater.from(App.context), R.layout.weather_fragment_recycler_header_one, parent, false);
+        } else if (viewType == TYPE_SECONDE) {
+            binding = DataBindingUtil.inflate(LayoutInflater.from(App.context), R.layout.weather_fragment_recycler_header_seconde, parent, false);
+        } else if (viewType == TYPE_THIRD) {
+            binding = DataBindingUtil.inflate(LayoutInflater.from(App.context), R.layout.weather_fragment_recycler_header_tride, parent, false);
+        } else {
+            binding = DataBindingUtil.inflate(LayoutInflater.from(App.context), R.layout.weather_fragment_recycler_items, parent, false);
+        }
         return new WeatherFragmentRecyclerHolder(binding);
     }
 
